6a200cf04bb50a4018faca79a972cfdf507ec108
Hugo Velarde <hugovela15@gmail.com>
2021-02-27 22:26:20 -0600

    MainPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainPageComponent } from './main-page/main-page.component';
 import { DrawComponent } from './draw/draw.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  },
   {
     path:'main',
     component: MainPageComponent
